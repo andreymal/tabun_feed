fbfba5950c09cecc727aef7ff07aa8a625f21ce0
andreymal <andriyano-31@mail.ru>
2015-11-27 14:31:14 +0300

    Rewriting from scratch (misc)

---
+++ Makefile
@@ -0,0 +1,27 @@
+.PHONY: clean-pyc clean-build clean docs
+
+clean: clean-build clean-pyc
+
+clean-build:
+	rm -fr build/
+	rm -fr dist/
+	rm -fr .eggs/
+	find . -name '*.egg-info' -exec rm -fr {} +
+	find . -name '*.egg' -exec rm -rf {} +
+
+clean-pyc:
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+	find . -name '__pycache__' -exec rm -fr {} +
+
+dist: clean
+	python setup.py sdist
+	ls -l dist
+
+install: clean
+	python setup.py install
+
+develop:
+	pip install -r requirements.txt
+	python setup.py develop
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tabun_feed',
+    version='0.6.0',
+    description='Watcher of new content on tabun.everypony.ru',
+    author='andreymal',
+    author_email='andriyano-31@mail.ru',
+    license='MIT',
+    url='https://github.com/andreymal/tabun_feed',
+    platforms=['linux', 'osx', 'bsd'],
+    packages=find_packages(),
+    install_requires=['tabun_api>=0.6.0'],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'tabun_feed=tabun_feed.runner:main',
+            'tf_manage=tabun_feed.manage:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
