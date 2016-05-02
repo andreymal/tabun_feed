ddc2fd207a949c2dbb276def50c5dea21f45d485
andreymal <andriyano-31@mail.ru>
2016-05-03 02:23:36 +0300

    Upgrade to tabun_api 0.7.0

--- tabun_feed/user.py
+++ tabun_feed/user.py
@@ -3,8 +3,6 @@
 
 from __future__ import unicode_literals
 
-import time
-
 import tabun_api as api
 
 from . import core, worker
@@ -13,76 +11,6 @@ from . import core, worker
 user = None
 anon = None
 last_requests = []
-
-
-def auth1():
-    """Проходит авторизацию на табуне с указанными в конфигурации параметрами.
-    Число попыток не ограничено: подвиснет интернет, упадёт сайт — функция дождётся, пока всё починится.
-    """
-    global user, anon
-    errors = 0
-
-    # авторизация пользователя
-    while True:
-        try:
-            user = None
-            # при наличии session_id логиниться, возможно, и не надо
-            if core.config.get('tabun_feed', 'session_id') and core.config.get('tabun_feed', 'password'):
-                tmpuser = api.User(phpsessid=core.config.get('tabun_feed', 'session_id'))
-                if tmpuser.username:
-                    if core.config.get('tabun_feed', 'username') == tmpuser.username:
-                        core.logger.info('Fast login!')
-                        user = tmpuser
-                    del tmpuser
-
-            if user is None:
-                user = api.User(
-                    phpsessid=core.config.get('tabun_feed', 'session_id'),
-                    login=core.config.get('tabun_feed', 'username'),
-                    passwd=core.config.get('tabun_feed', 'password'),
-                    http_host=core.config.get('tabun_feed', 'http_host'),
-                    session_cookie_name=core.config.get('tabun_feed', 'session_cookie_name'),
-                )
-            break
-        except api.TabunError as exc:
-            core.logger.error("user auth error: %s", exc.message)
-            errors += 1
-            # избегаем удара банхаммером от fail2ban
-            if errors % 3 == 0 or 'пароль' in exc.message:
-                time.sleep(60)
-            else:
-                time.sleep(5)
-
-    # запасной неавторизованный пользователь, чтоб, например, подсветку новых комментариев зазря не сбивать
-    if not user.username:
-        anon = user
-    else:
-        errors = 0
-        while True:
-            try:
-                anon = api.User(
-                    http_host=core.config.get('tabun_feed', 'http_host'),
-                    session_cookie_name=core.config.get('tabun_feed', 'session_cookie_name'),
-                )
-            except api.TabunError as exc:
-                core.logger.error("anon auth error: %s", exc.message)
-                errors += 1
-                if errors % 3 == 0:
-                    time.sleep(60)
-                else:
-                    time.sleep(5)
-            else:
-                break
-
-    if core.config.getfloat('tabun_feed', 'timeout') is not None:
-        user.timeout = core.config.getfloat('tabun_feed', 'timeout')
-        anon.timeout = user.timeout
-
-    user.query_interval = core.config.getfloat('tabun_feed', 'query_interval')
-    anon.query_interval = user.query_interval
-
-    core.logger.info("Logged in as %s", user.username or '[anonymous]')
-    worker.call_handlers("relogin_user")
 
 
 def auth(username=None, password=None, session_id=None, http_host=None, session_cookie_name=None, query_interval=None, timeout=None):
@@ -104,7 +32,7 @@ def auth(username=None, password=None, session_id=None, http_host=None, session_
             u = None
             # при наличии session_id логиниться, возможно, и не надо
             if session_id and password:
-                tmpuser = api.User(phpsessid=session_id)
+                tmpuser = api.User(session_id=session_id)
                 if tmpuser.username:
                     if username == tmpuser.username:
                         core.logger.info('Fast login %s!', username)
@@ -113,7 +41,7 @@ def auth(username=None, password=None, session_id=None, http_host=None, session_
 
             if u is None:
                 u = api.User(
-                    phpsessid=session_id,
+                    session_id=session_id,
                     login=username,
                     passwd=password,
                     http_host=http_host,
@@ -211,18 +139,13 @@ def open_with_check(url, timeout=None):
                 break
 
             try:
-                raw_data = user.urlopen(url, timeout=timeout).read()
+                raw_data = user.urlread(url, timeout=timeout)
                 if worker.status['request_error']:
                     worker.status['request_error'] = ''
                 break  # залогиненность проверяем в условии while
             except api.TabunError as exc:
                 worker.status['request_error'] = exc.message
                 # после десятой попытки или при выходе сдаёмся
-                if i >= 9 or worker.quit_event.is_set():
-                    raise
-                worker.quit_event.wait(3)
-            except IOError as exc:  # перехватывается в user.urlopen, только вот read() уже не там
-                worker.status['request_error'] = 'IOError ' + str(exc)
                 if i >= 9 or worker.quit_event.is_set():
                     raise
                 worker.quit_event.wait(3)
